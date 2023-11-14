06d249ddbdcd09c6b3009dd9cccbf5b0484f80f6
kralouf <114093052+kralouf@users.noreply.github.com>
2023-11-13 18:04:05 -0600

    Create linux-srv2.sh

---
+++ linux-srv2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create a directory named 'ideas' in the specified path
+mkdir -p /home/int-design/ideas
+
+# Create a file named 'ideas.txt' in the 'ideas' directory
+touch /home/int-design/ideas/ideas.txt
+
+# Write content to the file
+echo "Lots of ideas are in here..." > /home/int-design/ideas/ideas.txt
