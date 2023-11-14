20b1644110dda9e3ce5dcafbf2c3da13ac053e59
kralouf <114093052+kralouf@users.noreply.github.com>
2023-11-13 18:04:43 -0600

    Create winSRV.bat

---
+++ winSRV.bat
@@ -0,0 +1,7 @@
+@echo off
+
+:: Create a file named 'patient-data.txt' in the Documents folder
+echo There are things in here, hi > "C:\Users\Administrator\Documents\patient-data.txt"
+
+:: Create a directory named 'ARCHIVED' in the Administrator's home directory
+mkdir "C:\Users\Administrator\ARCHIVED"
